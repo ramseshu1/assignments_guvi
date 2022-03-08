897766430842f5e6b51ce3f9cf3418dae75900e5
ramseshu1 <49634683+ramseshu1@users.noreply.github.com>
2022-03-09 02:26:07 +0530

    Update guvi_assignment0Task1.py

--- guvi_assignment0Task1.py
+++ guvi_assignment0Task1.py
@@ -31,3 +31,35 @@ else:
     1. You don't have any constrains to input name.
     2.Your mobile number consists of numbers only and the maximum length must be exactly 10.
     3. Your gmail must be made out of a combination of alphabets and numbers,as well as must end with\\'@gmail.com\\'""")
+  
+  
+  # Create a file
+file=open("answer_Guvi.txt","a")
+
+# Take user input
+file.write("\n")
+
+text=input().split()                                                            
+
+# Capitalize each word and add to file
+temp=list(map(lambda x: x.capitalize()+ " ",text))                                
+
+file.writelines(temp)                                                             
+
+file.write("\n")                                                                
+
+
+# Mobile number as input
+number=input()                                                                     
+
+# Verify mobile number
+if len(number)==10 and number.isnumeric()==True:                                      
+  file.writelines(number)                                                          
+
+file.close()                                                                    
+
+
+# Open file in read mode, Print lines, Close the file
+file1=open("answer_Guvi.txt","r")                                                     
+print(file1.read())                                                             
+file.close()     
